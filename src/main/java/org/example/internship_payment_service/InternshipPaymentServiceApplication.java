f6a4b5e5d07c85f7b6b70517a73fc3d912c28ff6
danikmarukovitch <marukovitch3@icloud.com>
2026-08-24 17:56:36 +0200

    Initial commit: project skeleton

---
+++ src/main/java/org/example/internship_payment_service/InternshipPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.internship_payment_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternshipPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/internship_payment_service/InternshipPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.internship_payment_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
