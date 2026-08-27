f4cc1bad87ca258424313a95f0dc4aeb292debf7
danikmarukovitch <marukovitch3@icloud.com>
2026-08-27 10:45:36 +0200

    Add Payment entity and repository with CRUD

---
+++ src/main/java/org/example/internship_payment_service/entity/Payment.java
@@ -0,0 +1,39 @@
+package org.example.internship_payment_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "payments")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private PaymentStatus status;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "payment_amount", nullable = false, precision = 19, scale = 2)
+    private BigDecimal paymentAmount;
+
+}
---
+++ src/main/java/org/example/internship_payment_service/entity/PaymentStatus.java
@@ -0,0 +1,8 @@
+package org.example.internship_payment_service.entity;
+
+public enum PaymentStatus {
+    PENDING,
+    SUCCESS,
+    FAILED,
+    REFUNDED
+}
